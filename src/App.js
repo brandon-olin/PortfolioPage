d61e8be3a045e5abddad08e11db1a97e29eb9dfc
Brandon Olin <olin.brandon@gmail.com>
2021-02-15 09:12:08 -0700

    bootstrap added in project and imported in root SCSS file

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './styles/index.scss';
 function App() {
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <header className="container">
+        <h1 className="header">Hello world!</h1>
+      </header>
     </div>
   );
 }
