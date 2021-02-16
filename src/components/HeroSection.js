f68e504ee69563caf8537aa9a4efa1c1465c0ebb
Brandon Olin <olin.brandon@gmail.com>
2021-02-16 10:19:13 -0700

    Typed.js added for animated headline. Customized buttons rendering to view resume and contact me via email. Avatar image added for some extra visuals, background changed to code to make it less boring, and bouncing down arrow added to push viewer towards next piece of content

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
-import Button from '@material-ui/core/Button';
+import headshot from '../images/headshot.png';
+import Typed from 'react-typed';
 
 const HeroSection = () => {
+    // const options = {
+    //     strings: ["Hello there! I'm Brandon Olin", "Full-stack developer based in Denver, CO."],
+    // }
+
+    // const typed = new Typed('h1', )
     return (
         <div id="hero-container">
-            <div className="container" id="hero-text-container">
-                <h1 id="hero-title">Hello, I'm Brandon Olin. A full-stack developer based in Denver, CO.</h1>
-                <div id="hero-buttons">
-                    <a href="https://standardresume.co/r/_60ykSHwQVH_SwAZ0PTbo" id="resume-button">
-                        <Button variant="contained" color="primary" size="large">Resume</Button>
-                    </a>
-                    <a href="mailto:dev@brandonolin.com">
-                        <Button variant="contained" color="primary" size="large">Contact Me</Button>
-                    </a>
+            <div className="container" id="hero-content-container">
+                <div>
+                    <h1 id="hero-title">
+                        <Typed
+                            strings={["Hello there! I'm Brandon Olin.<br>^1000Full-stack developer based in Denver."]}
+                            typeSpeed={40}
+                        />
+                    </h1>
+                    <div id="hero-buttons">
+                        <a href="https://standardresume.co/r/_60ykSHwQVH_SwAZ0PTbo" id="resume-button">
+                            <button class="hero-button">RESUME</button>
+                        </a>
+                        <a href="mailto:dev@brandonolin.com" id="resume-button">
+                            <button class="hero-button">CONTACT ME</button>
+                        </a>
+                    </div>
                 </div>
+                <img src={headshot} alt="" id="avatar-image" />
             </div>
             <div className="arrow bounce">
                 <FontAwesomeIcon className="fa fa-arrow-down fa-2x" icon={faArrowDown} />
