4c712f012fa3c37d002c69c95c3b94343623fca0
Brandon Olin <olin.brandon@gmail.com>
2021-02-18 14:37:04 -0700

    Testimonials added and rendering along with avatar image for person who gave the testimonial. Also fixed default indentation size setting and cleaned up various files to reflect this

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,32 +5,32 @@ import headshot from '../images/headshot.png';
 import Typed from 'react-typed';
 
 const HeroSection = () => {
-    return (
-        <div id="hero-container">
-            <div className="container" id="hero-content-container">
-                <div>
-                    <h1 id="hero-title">
-                        <Typed
-                            strings={["Hello there!^500 I'm Brandon Olin.^500 <br>Full-stack developer based in Denver."]}
-                            typeSpeed={40}
-                        />
-                    </h1>
-                    <div id="hero-buttons">
-                        <a href="https://standardresume.co/r/_60ykSHwQVH_SwAZ0PTbo" id="resume-button">
-                            <button className="hero-button">RESUME</button>
-                        </a>
-                        <a href="mailto:dev@brandonolin.com" id="resume-button">
-                            <button className="hero-button">CONTACT ME</button>
-                        </a>
-                    </div>
-                </div>
-                <img src={headshot} alt="headshot" id="avatar-image" />
-            </div>
-            <a className="arrow bounce" href="#about">
-                <FontAwesomeIcon className="fa fa-arrow-down fa-3x" icon={faArrowDown} />
+  return (
+    <div id="hero-container">
+      <div className="container" id="hero-content-container">
+        <div>
+          <h1 id="hero-title">
+            <Typed
+              strings={["Hello there!^500 I'm Brandon Olin.^500 <br>Full-stack developer based in Denver."]}
+              typeSpeed={40}
+            />
+          </h1>
+          <div id="hero-buttons">
+            <a href="https://standardresume.co/r/_60ykSHwQVH_SwAZ0PTbo" id="resume-button">
+              <button className="hero-button">RESUME</button>
             </a>
+            <a href="mailto:dev@brandonolin.com" id="resume-button">
+              <button className="hero-button">CONTACT ME</button>
+            </a>
+          </div>
         </div>
-    )
+        <img src={headshot} alt="headshot" id="avatar-image" />
+      </div>
+      <a className="arrow bounce" href="#about">
+        <FontAwesomeIcon className="fa fa-arrow-down fa-3x" icon={faArrowDown} />
+      </a>
+    </div>
+  )
 }
 
 export default HeroSection;
